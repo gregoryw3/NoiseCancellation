e1fa62dc34b630e3e8574fed18a3e9379c69418e
gregoryw3 <greg.wainer@gmail.com>
2021-11-03 20:21:45 -0400

    Initial Commit

---
+++ Shared/NoiseCancellationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoiseCancellationApp.swift
+//  Shared
+//
+//  Created by Gregory Wainer on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NoiseCancellationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
